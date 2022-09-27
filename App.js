bf96a63ddc91fe23d095ca2a9bcd95c85f683d27
Mikaela Schröder <mikaela.schroder@myy.haaga-helia.fi>
2022-09-27 19:13:41 +0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [region,setRegion] = useState({
+    latitude:60.200692,
+    longitude:24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221,
+    
+  })
+  const[keyword, setKeyword] = useState('');
+
+  const getAddress = () => {
+    fetch('http://www.mapquestapi.com/geocoding/v1/address?key=6xTUGn75DFY9mRXJ6osTNeWGFyRJYLPW&location=${keyword}')
+      .then(response => response.json())
+      .then(data => setRegion({latitude: data.results[0].locations[0].latLng.lat, longitude: data.results[0].locations[0].latLng.lng }))
+      .catch(error => {
+      Alert.alert('Error', error)
+    })
+  }
+
+  
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView  style={{ flex: 4 }} 
+     region={region}>
+        <Marker coordinate={{
+          latitude:region.latitude, longitude:region.longitude
+          }}
+          title={keyword} />
+      </MapView>
+      <View style={{ flex: 1 }}>
+      <TextInput style={{ fontSize: 18, width: 200 }}
+       placeholder='insert address'
+       value={keyword}
+       onChangeText={value => setKeyword(value)}
+       ></TextInput>
+       <Button title='SHOW' onPress={getAddress} ></Button>
+       </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+ 
+ 
 });
